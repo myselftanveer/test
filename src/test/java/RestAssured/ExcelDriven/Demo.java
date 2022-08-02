7bdec60eb0375716074cb873e542db350f8e868d
tanveershaikh <myselftanveer@gmail.com>
2022-08-02 15:02:24 +0530

    demo

--- src/test/java/RestAssured/ExcelDriven/Demo.java
+++ src/test/java/RestAssured/ExcelDriven/Demo.java
@@ -7,7 +7,7 @@ public class Demo {
 	public static void main(String[] args) throws Exception {
 
 		DataDrivenUsingExcel data = new DataDrivenUsingExcel();
-		ArrayList arraylist =  data.getExcelData("Add Profile","Sheet1");
+		ArrayList<String> arraylist =  data.getExcelData("Add Profile","Sheet1");
 		System.out.println(arraylist.get(0));
 		System.out.println(arraylist.get(1));
 		System.out.println(arraylist.get(2));
